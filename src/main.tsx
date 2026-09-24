fa75ee6a6fdfe013d095232c4997b27e52ec0dbb
Yuri260881 <tinaev.yuri@yandex.ru>
2026-09-24 18:51:05 +0300

    Add app entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from './hooks/useAppData';
+import './index.css';
+
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container #root was not found');
+
+ReactDOM.createRoot(container).render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+);
